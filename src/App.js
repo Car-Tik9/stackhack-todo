b43c8b15951d7ab67989cf62cfa2796509828fe4
Karthik Uppala <karthikgupta999@gmail.com>
2020-05-19 21:31:54 +0530

    refs#master sign in initail commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Redirect, Route, Router, Switch } from "react-router-dom";
+import { createMuiTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
+import history from './utils/history'
+import SignIn from './components/SignIn'
+const theme = createMuiTheme();
 
 function App() {
   return (
-   <div>
-      Welcome to Stack Hack ..Happy Hacking
-     </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Router history={history}>
+            <Route exact path="/signin" component={SignIn}></Route>
+      </Router>
+    </ThemeProvider>
   );
 }
 
