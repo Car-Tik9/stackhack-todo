7839ce811eef471c8c105d24ecdbf4e7c42f950b
Karthik Uppala <karthikgupta999@gmail.com>
2020-05-20 10:07:55 +0530

    refs#master server file changes commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: [
-      'http://localhost:3000',
+      'http://localhost:3000','https://memoranda-todo.herokuapp.com/'
     ],
     credentials: true
   })
