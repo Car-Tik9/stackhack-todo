1873db1cf8c5ea3fec83f93066ad66d621ed9fcf
Karthik Uppala <karthikgupta999@gmail.com>
2020-05-21 11:31:45 +0530

    refs#develop dialog cancel button added commit

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
-
+import { Box, Button, Grid } from "@material-ui/core";
 //Material Components
 import FormControl from "@material-ui/core/FormControl";
-import TextField from "@material-ui/core/TextField";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
 import InputLabel from "@material-ui/core/InputLabel";
-import { Container, Grid, Button } from "@material-ui/core";
+import Select from "@material-ui/core/Select";
+import TextField from "@material-ui/core/TextField";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
 const ToDoForm = (props) => {
   const initialToDoState = {
@@ -15,7 +20,8 @@ const ToDoForm = (props) => {
     description: "",
     priority: "",
   };
-
+  const classes = useStyles();
+  console.log(classes);
   const [todo, setTodo] = useState(initialToDoState);
 
   const handleInputChange = (e) => {
@@ -33,64 +39,72 @@ const ToDoForm = (props) => {
     if (!todo.title || !todo.description || !todo.priority) return;
 
     props.addTodo(todo);
-	setTodo(initialToDoState);
-	props.isOpenDlg(false);
+    setTodo(initialToDoState);
+    props.isOpenDlg(false);
   };
 
   return (
-    <Container>
-      <form onSubmit={handleSubmit}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              label="Title"
-              variant="outlined"
-              type="text"
-              name="title"
-              value={todo.title}
+    <form onSubmit={handleSubmit}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            label="Title"
+            variant="outlined"
+            type="text"
+            name="title"
+            value={todo.title}
+            onChange={handleInputChange}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label="Description"
+            variant="outlined"
+            type="text"
+            name="description"
+            value={todo.description}
+            onChange={handleInputChange}
+            fullWidth
+            multiline
+            rows={5}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl variant="outlined">
+            <InputLabel id="priority">Priority</InputLabel>
+            <Select
+              size="small"
+              htmlFor="priority"
+              name="priority"
+              value={todo.priority}
+              autoWidth={true}
+              native
               onChange={handleInputChange}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label="Description"
-              variant="outlined"
-              type="text"
-              name="description"
-              value={todo.description}
-              onChange={handleInputChange}
-              fullWidth
-              multiline
-              rows={5}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormControl variant="outlined">
-              <InputLabel id="priority">Priority</InputLabel>
-              <Select
-                size="small"
-                htmlFor="priority"
-                name="priority"
-                value={todo.priority}
-                autoWidth={true}
-                native
-                onChange={handleInputChange}
-              >
-                <option value={1}>High</option>
-                <option value={2}>Medium</option>
-                <option value={3}>Low</option>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
+            >
+              <option value={1}>High</option>
+              <option value={2}>Medium</option>
+              <option value={3}>Low</option>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <Box display="flex" flexDirection="row-reverse">
             <Button type="submit" variant="contained" color="primary">
               Add To Do
             </Button>
-          </Grid>
+            <Button
+              onClick={() => props.isOpenDlg(false)}
+              className={classes.margin}
+              variant="outlined"
+              color="primary"
+            >
+              Cancel
+            </Button>
+          </Box>
         </Grid>
-      </form>
-    </Container>
+      </Grid>
+    </form>
   );
 };
 
