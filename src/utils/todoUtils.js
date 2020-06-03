6d4edf60c01a5bd89b6fa4edb16f8b571bf691e8
Karthik Uppala <karthikgupta999@gmail.com>
2020-06-03 23:42:30 +0530

    refs#develop labels icons added commiy

--- src/utils/todoUtils.js
+++ src/utils/todoUtils.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { Button, Chip } from "@material-ui/core";
+import ShoppingIcon from '@material-ui/icons/ShoppingCart'
+import WorkIcon from '@material-ui/icons/Work'
+import HomeIcon from '@material-ui/icons/Home'
+import PersonalIcon from '@material-ui/icons/Face'
+import OtherIcon from '@material-ui/icons/More'
 
 export const getPriorityButton = (priority) => {
   switch (priority) {
@@ -87,54 +92,24 @@ export const getChipLabel = (chipId) => {
   switch (chipId) {
     case 1:
       return (
-        <Chip
-          style={{ color: "#7fd9ff"}}
-          size="small"
-          variant="outlined"
-          label="Personal"
-        >
-        </Chip>
+       <PersonalIcon/>
       );
     case 2:
       return (
-        <Chip
-          size="small"
-          style={{ color: "#5185fc"}}
-          variant="outlined"
-          label="Shopping"
-        >
-        </Chip>
+        <ShoppingIcon/>
       );
     case 3:
       return (
-        <Chip
-          style={{ color: "#4dd778" }}
-          size="small"
-          variant="outlined"
-          label="Home"
-        >
-        </Chip>
+        <HomeIcon/>
       );
 
     case 4:
       return (
-        <Chip
-          style={{ color: "#4dd778" }}
-          size="small"
-          variant="outlined"
-          label="Work"
-        >
-        </Chip>
+        <WorkIcon/>
       );
     case 5:
       return (
-        <Chip
-          style={{ color: "#4dd778" }}
-          size="small"
-          variant="outlined"
-          label="Other"
-        >
-        </Chip>
+         <OtherIcon/>
       );
     default:
       return null
