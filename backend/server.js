253b66978f6b6c24beef1526db228cbcb74e2321
Karthik Uppala <karthikgupta999@gmail.com>
2020-11-17 14:13:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 const cookieParser = require('cookie-parser');
 const app = express();
 
-const dbUrl = 'mongodb+srv://todoAdmin:hack@todo@cluster0-u9h7j.mongodb.net/test?retryWrites=true&w=majority'
+const dbUrl = 'Your Connection Url'
 app.use(bodyParser.json());
 app.use(
   cors({
